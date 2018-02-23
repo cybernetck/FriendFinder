2a5a0a3711944e22099309cf7e4975080173d53e
cybernetck <mattswomack@gmail.com>
2018-02-23 10:50:51 -0600

    Setup Routes, and basic front end functionality

---
+++ app/data/friends.js
@@ -0,0 +1,13 @@
+var friendsArray = [
+    {
+      name: "Matt",
+      imgURL:"https://s19.postimg.org/u4m1awi2b/Matt_Womack_photo_2.jpg"
+    },
+    {
+        name: "Patt",
+        imgURL:"https://s19.postimg.org/9z51pcodv/Matt_Womack.jpg"
+    }
+  ];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = friendsArray;
--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 //NPM setup
 var express = require("express");
 var bodyParser = require("body-parser");
-var path = require("path");
+
+//Server and port
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // Routes
 // =============================================================
-require("./app/routing/apiRoutes.js");
+require("./app/routing/apiRoutes.js")(app);
 
 // Here we introduce HTML routing to serve different HTML files
-require("./app/routing/htmlRoutes.js");
+require("./app/routing/htmlRoutes.js")(app);
+
+//simplifies path stuffs
+// app.use(express.static(__dirname + '/app'));
 
 // Starts the server to begin listening
 // =============================================================
