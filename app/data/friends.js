d86126242f2d6e4b55512604aa0dc3da8a0d10fe
cybernetck <mattswomack@gmail.com>
2018-02-27 10:48:59 -0600

    friends.js and surveyHTML setup

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,13 +1,40 @@
 var friendsArray = [
-    {
-      name: "Matt",
-      imgURL:"https://s19.postimg.org/u4m1awi2b/Matt_Womack_photo_2.jpg"
-    },
-    {
-        name: "Patt",
-        imgURL:"https://s19.postimg.org/9z51pcodv/Matt_Womack.jpg"
-    }
-  ];
+  {
+    name: "Matt",
+    imgURL: "https://s19.postimg.org/u4m1awi2b/Matt_Womack_photo_2.jpg",
+    results: [5, 3, 2, 4, 1, 2, 1, 3, 2]
+  },
+  {
+    name: "Grandma Sarah",
+    imgURL: "Link",
+    results: [3, 1, 1, 5, 5, 5, 2, 1, 5]
+  },
+  {
+    name: "Emo Eric",
+    imgURL: "Link",
+    results: [3, 3, 3, 3, 3, 3, 3, 3, 3]
+  },
+  {
+    name: "Conner",
+    imgURL: "Link",
+    results: [1, 3, 4, 5, 3, 2, 1, 3, 1]
+  },
+  {
+    name: "Darius",
+    imgURL: "Link",
+    results: [5, 5, 5, 5, 5, 5, 5, 5, 1]
+  },
+  {
+    name: "Lisa",
+    imgURL: "Link",
+    results: [2, 5, 2, 4, 1, 1, 5, 4, 5]
+  },
+  {
+    name: "Micah",
+    imgURL: "Link",
+    results: [4, 1, 4, 1, 5, 1, 5, 1, 3]
+  }
+];
   
   // Note how we export the array. This makes it accessible to other files using require.
   module.exports = friendsArray;
