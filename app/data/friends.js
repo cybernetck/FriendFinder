0c23e359aec0f6f07e96725b57dc4c86a06b3f43
cybernetck <mattswomack@gmail.com>
2018-03-05 00:19:19 -0600

    Resolve presentation issues with modal and completed backend logic.

--- app/data/friends.js
+++ app/data/friends.js
@@ -6,40 +6,40 @@ var friendsArray = [
   },
   {
     name: "Grandma Sarah",
-    imgURL: "Link",
-    question: [3, 1, 1, 5, 5, 5, 2, 1, 5]
+    imgURL: "https://s19.postimg.org/6n8nr8t1f/gamer_Grandma.jpg",
+    question: [4, 1, 1, 5, 5, 5, 2, 1, 5]
   },
   {
     name: "Emo Eric",
-    imgURL: "Link",
+    imgURL: "https://s19.postimg.org/gkjokb8cz/emo_Eric.png",
     question: [3, 3, 3, 3, 3, 3, 3, 3, 3]
   },
   {
     name: "Conner",
-    imgURL: "Link",
+    imgURL: "https://s19.postimg.org/f6s1oloer/Conner.jpg",
     question: [1, 3, 4, 5, 3, 2, 1, 3, 1]
   },
   {
     name: "Darius",
-    imgURL: "Link",
+    imgURL: "https://s19.postimg.org/rwwa23orn/Darius.jpg",
     question: [5, 5, 5, 5, 5, 5, 5, 5, 1]
   },
   {
     name: "Lisa",
-    imgURL: "Link",
+    imgURL: "https://s19.postimg.org/dqgj6vlmb/Lisa.jpg",
     question: [2, 5, 2, 4, 1, 1, 5, 4, 5]
   },
   {
     name: "Micah",
-    imgURL: "Link",
+    imgURL: "https://s19.postimg.org/4wpmp6yhf/Micah.jpg",
     question: [4, 1, 4, 1, 5, 1, 5, 1, 3]
   },
   {
-    name: "Chester AKA 'The Chessman'",
-    imgURL: "Link",
+    name: "Jerald AKA 'The Chessman'",
+    imgURL: "https://s19.postimg.org/wwtq9gu83/Jerald.jpg",
     question: [1, 1, 1, 5, 1, 5, 1, 1, 5]
   }
 ];
   
   // Note how we export the array. This makes it accessible to other files using require.
-  module.exports = friendsArray;
+  module.exports = friendsArray
