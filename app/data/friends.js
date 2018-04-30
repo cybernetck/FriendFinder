e4e4521f8aff900523075d24cac1fed6b0368ef7
cybernetck <mattswomack@gmail.com>
2018-04-30 16:16:50 -0500

    update postimage image links

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,45 +1,44 @@
 var friendsArray = [
   {
     name: "Matt",
-    imgURL: "https://s19.postimg.org/u4m1awi2b/Matt_Womack_photo_2.jpg",
+    imgURL: "https://s19.postimg.cc/u4m1awi2b/Matt_Womack_photo_2.jpg",
     question: [5, 3, 2, 4, 1, 2, 1, 3, 2]
   },
   {
     name: "Grandma Sarah",
-    imgURL: "https://s19.postimg.org/6n8nr8t1f/gamer_Grandma.jpg",
+    imgURL: "https://s19.postimg.cc/6n8nr8t1f/gamer_Grandma.jpg",
     question: [4, 1, 1, 5, 5, 5, 2, 1, 5]
   },
   {
     name: "Emo Eric",
-    imgURL: "https://s19.postimg.org/gkjokb8cz/emo_Eric.png",
+    imgURL: "https://s19.postimg.cc/gkjokb8cz/emo_Eric.png",
     question: [3, 3, 3, 3, 3, 3, 3, 3, 3]
   },
   {
     name: "Conner",
-    imgURL: "https://s19.postimg.org/f6s1oloer/Conner.jpg",
+    imgURL: "https://s19.postimg.cc/f6s1oloer/Conner.jpg",
     question: [1, 3, 4, 5, 3, 2, 1, 3, 1]
   },
   {
     name: "Darius",
-    imgURL: "https://s19.postimg.org/rwwa23orn/Darius.jpg",
+    imgURL: "https://s19.postimg.cc/rwwa23orn/Darius.jpg",
     question: [5, 5, 5, 5, 5, 5, 5, 5, 1]
   },
   {
     name: "Lisa",
-    imgURL: "https://s19.postimg.org/dqgj6vlmb/Lisa.jpg",
+    imgURL: "https://s19.postimg.cc/dqgj6vlmb/Lisa.jpg",
     question: [2, 5, 2, 4, 1, 1, 5, 4, 5]
   },
   {
     name: "Micah",
-    imgURL: "https://s19.postimg.org/4wpmp6yhf/Micah.jpg",
+    imgURL: "https://s19.postimg.cc/4wpmp6yhf/Micah.jpg",
     question: [4, 1, 4, 1, 5, 1, 5, 1, 3]
   },
   {
     name: "Jerald AKA 'The Chessman'",
-    imgURL: "https://s19.postimg.org/wwtq9gu83/Jerald.jpg",
+    imgURL: "https://s19.postimg.cc/wwtq9gu83/Jerald.jpg",
     question: [1, 1, 1, 5, 1, 5, 1, 1, 5]
   }
 ];
   
-  // Note how we export the array. This makes it accessible to other files using require.
   module.exports = friendsArray
